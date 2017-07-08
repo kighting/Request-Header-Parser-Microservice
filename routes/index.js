d1821d5f56cd8eabe894be5feb2d228600d24922
kaitinghuang <kightingh@gmail.com>
2017-07-08 16:16:17 -0700

    add ip request

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,13 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/api/whoami', function(req, res) {
-  console.log(res);
+  //console.log(res);
+  var ip = req.socket.remoteAddress;
+  //host = request.host //If just wanna show localhost
   var language = req.headers['accept-language'];
   var userAgent = req.headers['user-agent'];
   res.json({ 
+      "ip address": ip,
       "language": language,
       "operating system": userAgent
   });
