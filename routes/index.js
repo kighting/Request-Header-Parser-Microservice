968cc8148b00330537e4c8616218cff54031ab43
kaitinghuang <kightingh@gmail.com>
2017-07-08 15:18:07 -0700

    edit stylesheet

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/api/whoami', function(req, res, next) {
+  res.json({ key: 'value' });
+});
+
 module.exports = router;
