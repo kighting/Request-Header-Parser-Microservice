57eafa26922a8e59cfe4bb17a6949372ba8990d6
kaitinghuang <kightingh@gmail.com>
2017-07-08 20:41:03 -0700

    use regular expression to modify the output

--- routes/index.js
+++ routes/index.js
@@ -7,15 +7,15 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/api/whoami', function(req, res) {
-  //console.log(res);
-  var ip = req.socket.remoteAddress;
+  console.log(res);
+  var ip = req.socket.remoteAddress.replace(/^[\s\S]*:/, "");
   //host = request.host //If just wanna show localhost
-  var language = req.headers['accept-language'];
-  var userAgent = req.headers['user-agent'];
+  var language = req.headers['accept-language'].replace(/,[\s\S]*$/, "");
+  var userAgent = req.headers['user-agent'].match(/\(([^)]+)\)/);
   res.json({ 
       "ip address": ip,
       "language": language,
-      "operating system": userAgent
+      "operating system": userAgent[0].replace(/[\(\)]/g, "")
   });
 });
 
